f6438dd37f97b77f5eaef20a5ae1deaf28da5225
Hung-2006 <vovanhung1602@gmail.com>
2026-03-20 15:33:06 +0700

    Update bai131.py

--- bai131.py
+++ bai131.py
@@ -1,14 +1,13 @@
+import re
+
 chuoi = input("Nhap chuoi: ")
 
-# a. Kiểm tra đối xứng - chỉ xét chữ và số
-import re
 chuan = re.sub(r'[^a-zA-Z0-9]', '', chuoi).lower()
 if chuan == chuan[::-1]:
     print("Chuoi doi xung")
 else:
     print("Chuoi khong doi xung")
 
-# b. Xóa ký tự
 ky_tu = input("Xoa ky tu nao? ")
 ket_qua = ""
 for ch in chuoi:
