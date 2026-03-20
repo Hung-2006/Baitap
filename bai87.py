512cfb810bb641400d8a2779b43c31835aebcedc
Hung-2006 <vovanhung1602@gmail.com>
2026-03-20 15:30:19 +0700

    Update bai87.py

--- bai87.py
+++ bai87.py
@@ -1,6 +1,5 @@
 n = int(input("Nhap bac ma tran: "))
 
-# Nhập ma trận
 a = []
 for i in range(n):
     row = []
@@ -9,21 +8,17 @@ for i in range(n):
         row.append(x)
     a.append(row)
 
-# In ma trận
 print("Ma tran:")
 for i in range(n):
     for j in range(n):
         print(a[i][j], end=" ")
     print()
 
-# ===== Tính trace =====
 trace = 0
 for i in range(n):
     trace += a[i][i]
-
 print("Trace =", trace)
 
-# ===== Kiểm tra tam giác trên =====
 tam_giac_tren = True
 for i in range(n):
     for j in range(i):
@@ -33,7 +28,6 @@ for i in range(n):
     if not tam_giac_tren:
         break
 
-# ===== Nếu là tam giác trên thì tính định thức =====
 if tam_giac_tren:
     det = 1
     for i in range(n):
